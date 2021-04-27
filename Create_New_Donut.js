992687d1fd72d0989ec9174a439d0b12ca646a59
Daniel Thorson <81667518+DanielThorson@users.noreply.github.com>
2021-04-26 17:19:55 -0700

    Add files via upload

--- Create_New_Donut.js
+++ Create_New_Donut.js
@@ -1,27 +1,25 @@
-  // for creating new donuts
-  var n = [];
+// for creating new donuts
+var n = [];
 
+let btn12 = document.querySelector(".donut_create");
 
-  let btn12 = document.querySelector(".donut_create");
-  
-  btn12.addEventListener("click", function (event) {
-    let type = prompt("What is the name of the shop you want to create?");
-    let price = prompt("How much does it cost?");
-    alert("New Donut Type Created.");
-   n.push(type, Number.parseInt(price));
-    fetch("https://donutshop-api.herokuapp.com/create-donut-type?id=234", {
+btn12.addEventListener("click", function (event) {
+  let type = prompt("What is the name of the donut you want to create?");
+  let price = prompt("How much does it cost?");
+  alert("New Donut Type Created.");
+  n.push(type, Number.parseInt(price));
+  fetch("https://donutshop-api.herokuapp.com/create-donut-type?id=234", {
     method: "POST",
-    mode: 'cors',
+    mode: "cors",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
-      body: JSON.stringify({type, price}),
-    })
-      .then((response) => response.json())
-      .then((data) => n.push(data))
-      .then(updateInventory) //add this to every potst
-      .catch((err) => {
+    body: JSON.stringify({ type, price }),
+  })
+    .then((response) => response.json())
+    .then((data) => n.push(data))
+    .then(updateInventory) //add this to every potst
+    .catch((err) => {
       console.error(err);
     });
-  });
-  
+});
