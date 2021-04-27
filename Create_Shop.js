83e5db43828eaa7defc0464e66fdcd405df8ce5e
Daniel Thorson <81667518+DanielThorson@users.noreply.github.com>
2021-04-26 19:25:28 -0700

    Add files via upload

--- Create_Shop.js
+++ Create_Shop.js
@@ -1,20 +1,45 @@
+var h = [];
+
 let btn10 = document.querySelector(".create_shop");
 
 btn10.addEventListener("click", function (event) {
   let name = prompt("What is the name of your the shop?");
   alert("New Donut Shop Created.");
+  h.push(name);
   fetch("https://donutshop-api.herokuapp.com/create-donut-shop", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(new shop(name))
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-     getInput()
-  .catch((err) => {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name }),
+  })
+    .then((response) => response.json())
+    .then((data) => h.push(data));
+  getInput().catch((err) => {
     console.error(err);
   });
   alert("Donut Shop Created Successfully");
 });
+
+// let btn34 = document.querySelector(".donut_add");
+
+// btn34.addEventListener("click", function (event) {
+//   let type = prompt("Which type do you want to add?");
+//   let count = prompt("How many do you want to add?");
+//   alert("inventory change complete");
+//  h.push(name);
+//   fetch("https://donutshop-api.herokuapp.com/add-donuts?id=234", {
+//   method: "POST",
+//   mode: 'cors',
+//   headers: {
+//     "Content-Type": "application/json"
+//   },
+//     body: JSON.stringify({type, count}),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => h.push(data))
+//     .then(updateInventory) //add this to every potst
+//     .catch((err) => {
+//     console.error(err);
+//   });
+// });
